cb19adf4cb44f6c1525b672d529e6c17b6e738d5
sjvargas <sjvargas@uc.cl>
2015-10-12 23:54:23 -0300

    creacion admin academico
    ultima actualizacion

---
+++ src/g4/Administrador_academico.java
@@ -0,0 +1,67 @@
+package g4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * 
+ * 
+ * 12.	Clase Administrador_academico:
+ *  Clase que no tiene atributos, puede realizar todas las modificaciones a las mallas y cursos,
+ *   además de fijar los límites de créditos de la universidad.
+ * 
+ */
+
+public class Administrador_academico {
+	
+	
+	
+	private List<Programacion_Academica> historial_de_progrmacion_academica;
+	
+	private List<Carrera> lista_carreras;
+	private List<Ramo> lista_ramos;
+	private List<Profesor> lista_profesores;
+
+	private List<Alumno> lista_alumnos_restringidos;
+	
+	
+	public Administrador_academico(){
+		historial_de_progrmacion_academica = new ArrayList<Programacion_Academica>();
+		lista_carreras = new ArrayList<Carrera>();
+		lista_profesores = new ArrayList<Profesor>();
+		lista_ramos = new ArrayList<Ramo>();
+	}
+	
+	public void crear_programacion_academica(String periodo){
+		Programacion_Academica nueva_progra = new Programacion_Academica(periodo);
+	}
+	
+	public void agregar_carrera(String decano, String facultad){
+		
+		Carrera nuevaCarrera = new Carrera(lista_carreras.size()+1,decano,facultad);
+		
+		lista_carreras.add(nuevaCarrera);
+	}
+	
+	public void agregar_ramo(int id,String sigla,Carrera carrera, int creditos, String semestre_impartido,String contenidos,String objetivos){
+		
+
+			Ramo nuevoRamo = new Ramo(id, sigla, creditos, carrera, semestre_impartido, contenidos, objetivos);
+			lista_ramos.add(nuevoRamo);
+			
+	}
+	
+	public void agregar_profesor(String nombre, int sueldo, String facultad){
+		Profesor nuevoProfesor = new Profesor(lista_profesores.size()+1, nombre, sueldo, facultad);
+		
+		lista_profesores.add(nuevoProfesor);
+	}
+	
+	public void restringir_acceso_alumno(Alumno alumno){
+		
+		lista_alumnos_restringidos.add(alumno);
+		
+	}
+	
+
+}
