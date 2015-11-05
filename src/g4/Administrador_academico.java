d01e58d8757597b4424e824319a1d0d4054e5e4d
josehj <jhidalgo1@uc.cl>
2015-11-05 19:04:13 -0300

    cambios de consistencia

--- src/g4/Administrador_academico.java
+++ src/g4/Administrador_academico.java
@@ -2,6 +2,7 @@ package g4;
 
 import java.util.ArrayList;
 import java.util.List;
+import fx.view.main;
 
 /*
  * 
@@ -18,22 +19,12 @@ public class Administrador_academico extends Usuario {
 	
 	private List<Programacion_Academica> historial_de_progrmacion_academica;
 	
-	private List<Carrera> lista_carreras;
-	private List<Ramo> lista_ramos;
-	private List<Profesor> lista_profesores;
-	private List<Alumno> lista_alumnos_restringidos;
-	
 	
 	public Administrador_academico(String nom, String cont, String sex, int ed){
 		
 		
 		super(nom, cont, sex, ed);
-		
-
-		historial_de_progrmacion_academica = new ArrayList<Programacion_Academica>();
-		lista_carreras = new ArrayList<Carrera>();
-		lista_profesores = new ArrayList<Profesor>();
-		lista_ramos = new ArrayList<Ramo>();
+	
 	}
 	
 	public void crear_programacion_academica(String periodo){
@@ -41,38 +32,35 @@ public class Administrador_academico extends Usuario {
 	}
 	
 	public void agregar_carrera(String decano, String facultad, String nombre){
-		
-		Carrera nuevaCarrera = new Carrera(lista_carreras.size()+1,decano,facultad, nombre);
-		
-		lista_carreras.add(nuevaCarrera);
+		main.U.lista_carreras.add(new Carrera(decano,facultad, nombre));
 	}
 	
 	public void agregar_ramo(String sigla,Carrera carrera, int creditos, String semestre_impartido,String contenidos,String objetivos){
 		
 
 			Ramo nuevoRamo = new Ramo( sigla, creditos, carrera, semestre_impartido, contenidos, objetivos);
-			lista_ramos.add(nuevoRamo);
+			main.U.lista_ramos.add(nuevoRamo);
 			
 	}
 	
 	public void agregar_profesor(String nombre, String apellidos, int sueldo, String facultad){
-		Profesor nuevoProfesor = new Profesor(lista_profesores.size()+1, nombre, apellidos, sueldo, facultad);
+		Profesor nuevoProfesor = new Profesor( nombre, apellidos, sueldo, facultad);
 		
-		lista_profesores.add(nuevoProfesor);
+		main.U.lista_profesores.add(nuevoProfesor);
 	}
 	
 	public void restringir_acceso_alumno(Alumno alumno){
 		
-		lista_alumnos_restringidos.add(alumno);
+		alumno.acceso = false;
 		
 	}
 	
 	public List<Carrera> getListaCarrera(){
-		return this.lista_carreras;
+		return main.U.lista_carreras;
 	}
 	
 	public Carrera GetCarrera(int id_Carrera){
-		for(Carrera c : lista_carreras){
+		for(Carrera c : main.U.lista_carreras){
 			if(c.getId_carrera() == id_Carrera){
 				return c;
 			}
@@ -81,7 +69,7 @@ public class Administrador_academico extends Usuario {
 	}
 	
 	public Carrera GetCarrera(String nombreCarrera){
-		for(Carrera c : lista_carreras){
+		for(Carrera c : main.U.lista_carreras){
 			if(c.getnombre_carrera() == nombreCarrera){
 				return c;
 			}
