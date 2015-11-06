fd9ae91f27a58457e76edc27db7ba922e049f618
josehj <jhidalgo1@uc.cl>
2015-11-06 20:06:57 -0300

    busca curso terminado :100:

--- src/fx/view/BuscadorDeCursosController.java
+++ src/fx/view/BuscadorDeCursosController.java
@@ -70,7 +70,7 @@ public class BuscadorDeCursosController implements PrincipalController {
 	@FXML
 	private ObservableList<CursoTabla> data_busqueda = FXCollections.observableArrayList();
 	@FXML
-	private String[] horarios_buscar;
+	private ArrayList<String> horario_buscar = new ArrayList<String>();
 	@FXML
 	private CheckBox lu_1,lu_2,lu_3,lu_4,lu_5,lu_6,lu_7,lu_8; 
 	@FXML
@@ -115,62 +115,62 @@ public class BuscadorDeCursosController implements PrincipalController {
 		nombre_busqueda.setCellValueFactory(new PropertyValueFactory<CursoTabla,String>("nombre"));
 		profesor_busqueda.setCellValueFactory(new PropertyValueFactory<CursoTabla,String>("profesores"));
 		horario_busqueda.setCellValueFactory(new PropertyValueFactory<CursoTabla,String>("horarios"));
-		nombre_busqueda.setCellValueFactory(new PropertyValueFactory<CursoTabla,String>("nombre"));
+		carrera_busqueda.setCellValueFactory(new PropertyValueFactory<CursoTabla,String>("carrera"));
 		tabla_busqueda.setItems(data_busqueda);
 		
-		lu_1 = new CheckBox("lu:1");
-		lu_2 = new CheckBox("lu:2");
-		lu_3 = new CheckBox("lu:3");
-		lu_4 = new CheckBox("lu:4");
-		lu_5 = new CheckBox("lu:5");
-		lu_6 = new CheckBox("lu:6");
-		lu_7 = new CheckBox("lu:7");
-		lu_8 = new CheckBox("lu:8");
+		lu_1.setText("lu:1");
+		lu_2.setText("lu:2");
+		lu_3.setText("lu:3");
+		lu_4.setText("lu:4");
+		lu_5.setText("lu:5");
+		lu_6.setText("lu:6");
+		lu_7.setText("lu:7");
+		lu_8.setText("lu:8");
 		
-		ma_1 = new CheckBox("ma:1");
-		ma_2 = new CheckBox("ma:2");
-		ma_3 = new CheckBox("ma:3");
-		ma_4 = new CheckBox("ma:4");
-		ma_5 = new CheckBox("ma:5");
-		ma_6 = new CheckBox("ma:6");
-		ma_7 = new CheckBox("ma:7");
-		ma_8 = new CheckBox("ma:8");
+		ma_1.setText("ma:1");
+		ma_2.setText("ma:2");
+		ma_3.setText("ma:3");
+		ma_4.setText("ma:4");
+		ma_5.setText("ma:5");
+		ma_6.setText("ma:6");
+		ma_7.setText("ma:7");
+		ma_8.setText("ma:8");
 		
-		mi_1 = new CheckBox("mi:1");
-		mi_2 = new CheckBox("mi:2");
-		mi_3 = new CheckBox("mi:3");
-		mi_4 = new CheckBox("mi:4");
-		mi_5 = new CheckBox("mi:5");
-		mi_6 = new CheckBox("mi:6");
-		mi_7 = new CheckBox("mi:7");
-		mi_8 = new CheckBox("mi:8");
+		mi_1.setText("mi:1");
+		mi_2.setText("mi:2");
+		mi_3.setText("mi:3");
+		mi_4.setText("mi:4");
+		mi_5.setText("mi:5");
+		mi_6.setText("mi:6");
+		mi_7.setText("mi:7");
+		mi_8.setText("mi:8");
 		
-		ju_1 = new CheckBox("ju:1");
-		ju_2 = new CheckBox("ju:2");
-		ju_3 = new CheckBox("ju:3");
-		ju_4 = new CheckBox("ju:4");
-		ju_5 = new CheckBox("ju:5");
-		ju_6 = new CheckBox("ju:6");
-		ju_7 = new CheckBox("ju:7");
-		ju_8 = new CheckBox("ju:8");
+		ju_1.setText("ju:1");
+		ju_2.setText("ju:2");
+		ju_3.setText("ju:3");
+		ju_4.setText("ju:4");
+		ju_5.setText("ju:5");
+		ju_6.setText("ju:6");
+		ju_7.setText("ju:7");
+		ju_8.setText("ju:8");
 		
-		vi_1 = new CheckBox("vi:1");
-		vi_2 = new CheckBox("vi:2");
-		vi_3 = new CheckBox("vi:3");
-		vi_4 = new CheckBox("vi:4");
-		vi_5 = new CheckBox("vi:5");
-		vi_6 = new CheckBox("vi:6");
-		vi_7 = new CheckBox("vi:7");
-		vi_8 = new CheckBox("vi:8");
+		vi_1.setText("vi:1");
+		vi_2.setText("vi:2");
+		vi_3.setText("vi:3");
+		vi_4.setText("vi:4");
+		vi_5.setText("vi:5");
+		vi_6.setText("vi:6");
+		vi_7.setText("vi:7");
+		vi_8.setText("vi:8");
 		
-		sa_1 = new CheckBox("sa:1");
-		sa_2 = new CheckBox("sa:2");
-		sa_3 = new CheckBox("sa:3");
-		sa_4 = new CheckBox("sa:4");
-		sa_5 = new CheckBox("sa:5");
-		sa_6 = new CheckBox("sa:6");
-		sa_7 = new CheckBox("sa:7");
-		sa_8 = new CheckBox("sa:8");
+		sa_1.setText("sa:1");
+		sa_2.setText("sa:2");
+		sa_3.setText("sa:3");
+		sa_4.setText("sa:4");
+		sa_5.setText("sa:5");
+		sa_6.setText("sa:6");
+		sa_7.setText("sa:7");
+		sa_8.setText("sa:8");
 		
 		
 		
@@ -188,64 +188,10 @@ public class BuscadorDeCursosController implements PrincipalController {
 	}
 	
 	public void ClickEliminar(ActionEvent event) {
-		controlador.setScreen(main.AlumnoID);
+		data_seleccion.remove(tabla_busqueda.getSelectionModel().getSelectedItem());
+		ActualizarSeleccion();
 	}	
 	public void ClickBuscar(ActionEvent event) {
-		ArrayList<String> horario_buscar = new ArrayList<String>();
-		if(lu_1.isSelected()){horario_buscar.add(lu_1.getText());}
-		if(lu_2.isSelected()){horario_buscar.add(lu_2.getText());}
-		if(lu_3.isSelected()){horario_buscar.add(lu_3.getText());}
-		if(lu_4.isSelected()){horario_buscar.add(lu_4.getText());}
-		if(lu_5.isSelected()){horario_buscar.add(lu_5.getText());}
-		if(lu_6.isSelected()){horario_buscar.add(lu_6.getText());}
-		if(lu_7.isSelected()){horario_buscar.add(lu_7.getText());}
-		if(lu_8.isSelected()){horario_buscar.add(lu_8.getText());}
-
-		if(ma_1.isSelected()){horario_buscar.add(ma_1.getText());}
-		if(ma_2.isSelected()){horario_buscar.add(ma_2.getText());}
-		if(ma_3.isSelected()){horario_buscar.add(ma_3.getText());}
-		if(ma_4.isSelected()){horario_buscar.add(ma_4.getText());}
-		if(ma_5.isSelected()){horario_buscar.add(ma_5.getText());}
-		if(ma_6.isSelected()){horario_buscar.add(ma_6.getText());}
-		if(ma_7.isSelected()){horario_buscar.add(ma_7.getText());}
-		if(ma_8.isSelected()){horario_buscar.add(ma_8.getText());}
-
-		if(mi_1.isSelected()){horario_buscar.add(mi_1.getText());}
-		if(mi_2.isSelected()){horario_buscar.add(mi_2.getText());}
-		if(mi_3.isSelected()){horario_buscar.add(mi_3.getText());}
-		if(mi_4.isSelected()){horario_buscar.add(mi_4.getText());}
-		if(mi_5.isSelected()){horario_buscar.add(mi_5.getText());}
-		if(mi_6.isSelected()){horario_buscar.add(mi_6.getText());}
-		if(mi_7.isSelected()){horario_buscar.add(mi_7.getText());}
-		if(mi_8.isSelected()){horario_buscar.add(mi_8.getText());}
-
-		if(ju_1.isSelected()){horario_buscar.add(ju_1.getText());}
-		if(ju_2.isSelected()){horario_buscar.add(ju_2.getText());}
-		if(ju_3.isSelected()){horario_buscar.add(ju_3.getText());}
-		if(ju_4.isSelected()){horario_buscar.add(ju_4.getText());}
-		if(ju_5.isSelected()){horario_buscar.add(ju_5.getText());}
-		if(ju_6.isSelected()){horario_buscar.add(ju_6.getText());}
-		if(ju_7.isSelected()){horario_buscar.add(ju_7.getText());}
-		if(ju_8.isSelected()){horario_buscar.add(ju_8.getText());}
-
-		if(vi_1.isSelected()){horario_buscar.add(vi_1.getText());}
-		if(vi_2.isSelected()){horario_buscar.add(vi_2.getText());}
-		if(vi_3.isSelected()){horario_buscar.add(vi_3.getText());}
-		if(vi_4.isSelected()){horario_buscar.add(vi_4.getText());}
-		if(vi_5.isSelected()){horario_buscar.add(vi_5.getText());}
-		if(vi_6.isSelected()){horario_buscar.add(vi_6.getText());}
-		if(vi_7.isSelected()){horario_buscar.add(vi_7.getText());}
-		if(vi_8.isSelected()){horario_buscar.add(vi_8.getText());}
-
-		if(sa_1.isSelected()){horario_buscar.add(sa_1.getText());}
-		if(sa_2.isSelected()){horario_buscar.add(sa_2.getText());}
-		if(sa_3.isSelected()){horario_buscar.add(sa_3.getText());}
-		if(sa_4.isSelected()){horario_buscar.add(sa_4.getText());}
-		if(sa_5.isSelected()){horario_buscar.add(sa_5.getText());}
-		if(sa_6.isSelected()){horario_buscar.add(sa_6.getText());}
-		if(sa_7.isSelected()){horario_buscar.add(sa_7.getText());}
-		if(sa_8.isSelected()){horario_buscar.add(sa_8.getText());}
-
 		String prof = text_profesor.getText();
 		String sigla = text_sigla.getText();
 		int carrera = -1;
@@ -258,16 +204,7 @@ public class BuscadorDeCursosController implements PrincipalController {
 		ArrayList<Curso> cursos = main.U.buscador.filtrar(horario_buscar, prof, carrera, sigla,periodo);
 		ArrayList<CursoTabla> ct = new ArrayList<CursoTabla>();
 		for (Curso j : cursos){
-			System.out.println(j.getRamo().getSigla());
 			ct.add(new CursoTabla(j));
-		}
-		for (CursoTabla j : ct){
-			System.out.println(j.getNombre());
-			System.out.println(j.getSigla());
-			System.out.println(j.getProfesores());
-			System.out.println(j.getHorarios());
-			System.out.println(j.getSeccion());
-			System.out.println(j.getCarrera());
 		}
 		data_busqueda = FXCollections.observableArrayList(ct);
 		ActualizarBusqueda();
@@ -275,7 +212,8 @@ public class BuscadorDeCursosController implements PrincipalController {
 	
 	
 	public void ClickAgregar(ActionEvent event) {
-		System.out.print(tabla_busqueda.getSelectionModel().getSelectedItem());
+		data_seleccion.add(tabla_busqueda.getSelectionModel().getSelectedItem());
+		ActualizarSeleccion();
 	}
 	
 	
@@ -292,11 +230,18 @@ public class BuscadorDeCursosController implements PrincipalController {
 	
 	
 	public void ActualizarSeleccion(){
-		tabla_busqueda.setItems(data_seleccion);
+		tabla_seleccion.setItems(data_seleccion);
 	}
 	
-	
-	
-	
-	
+	public void ClickCheckbox(ActionEvent e){
+		if (e.getSource() instanceof CheckBox) {
+			CheckBox chk = (CheckBox) e.getSource();
+			if (horario_buscar.contains(chk.getText())) {
+				horario_buscar.remove(chk.getText());
+            } else {
+            	horario_buscar.add(chk.getText());
+            }
+		}
+	}
+
 }
